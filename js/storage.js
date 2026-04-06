9ad33c446715aae6fd6e8345471b5d2c307b483d
Manya Jain <manyajain3690@gmail.com>
2026-04-06 08:21:10 +0530

    Create storage.js

---
+++ js/storage.js
@@ -0,0 +1,11 @@
+```javascript id="store001"}
+function saveReport(r){
+let arr=JSON.parse(localStorage.getItem("reports"))||[];
+arr.push(r);
+localStorage.setItem("reports",JSON.stringify(arr));
+}
+
+function getReports(){
+return JSON.parse(localStorage.getItem("reports"))||[];
+}
+```
