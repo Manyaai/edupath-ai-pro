3be454e6770f2d1402b44ff21f6a9ef0a5874a54
Manya Jain <manyajain3690@gmail.com>
2026-04-06 08:24:58 +0530

    Add compareCareers function to compare.js

---
+++ js/compare.js
@@ -0,0 +1,11 @@
+```javascript id="comp001"}
+function compareCareers(careers){
+let html="<h2>Comparison</h2>";
+
+careers.forEach(c=>{
+html+=`<p>${c.name} - ${c.growth}</p>`;
+});
+
+compare.innerHTML=html;
+}
+```
